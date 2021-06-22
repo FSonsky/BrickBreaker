38115a7a17a05869e4b9fe29b3b4c3f4d236222a
Winglet80 <frederik@sonsky.se>
2021-06-22 14:12:40 +0200

    Added terminal object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        //TerminalSize ts = new TerminalSize(400, 420);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        //terminalFactory.setInitialTerminalSize(ts);
+        Terminal terminal = terminalFactory.createTerminal();
+        terminal.setCursorVisible(false);
     }
 }
